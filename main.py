b1134c72def51e82f3bde633ef7376fd96c496aa
jewelangelo <82922873+jewelangelo@users.noreply.github.com>
2023-01-27 08:34:26 +0000

    modified:   README.md 	new file:   analysis.ipynb 	modified:   bts_trends.ipynb 	modified:   data/bts_trends.csv 	new file:   data/data.csv 	modified:   data/world_population.csv 	deleted:    main.py 	modified:   world_pop.ipynb

--- main.py
+++
@@ -1,9 +0,0 @@
-import kaggle
-
-kaggle datasets list -s 'world by population 2022'
-
-kaggle datasets download -d "anandhuh/countries-in-the-world-by-population-2022"
-
-mv /workspaces/bts_jewel/"countries-in-the-world-by-population-2022.zip" /workspaces/bts_jewel/downloads/
-
-unzip /workspaces/bts_jewel/downloads/"countries-in-the-world-by-population-2022.zip" -d data/
